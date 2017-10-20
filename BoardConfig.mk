455eb0b1664d0493ab9d2c1b5bdcbc983a3dbe3a
filiprrs <filiprrs@gerrit.twrp.me>
2017-10-20 16:27:07 +0000

    t0lte: adjust twrp settings
    Change-Id: I7e05ff703e1db8edea3f6a54532afba58594911a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,18 +23,18 @@
 
 # GPS
 BOARD_GPS_SET_PRIVACY := true
+
+#Compress recovery with LZMA
 LZMA_RAMDISK_TARGETS := boot,recovery 
 
-#twrp
-TARGET_RECOVERY_FSTAB := device/samsung/t0lte/recovery/twrp.fstab
-TW_THEME := portrait_hdpi
+#TWRP
+TW_INCLUDE_CRYPTO := true
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
+TW_MAX_BRIGHTNESS := 255
 RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
 TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
-TW_INCLUDE_CRYPTO := true
-TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TW_NO_USB_STORAGE := true
-TW_MAX_BRIGHTNESS := 255
-TW_NO_EXFAT_FUSE := true
-TW_NO_EXFAT := true
-TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
+TW_THEME := portrait_hdpi
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_EXCLUDE_SUPERSU := true
